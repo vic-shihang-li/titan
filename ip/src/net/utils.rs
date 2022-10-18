3b10cd17193661476287106a3b20a57a4f580542
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-10-18 12:03:42 -0400

    fix localhost addr

--- ip/src/net/utils.rs
+++ ip/src/net/utils.rs
@@ -1,5 +1,5 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 pub fn localhost_with_port(port: u16) -> SocketAddr {
-    SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::new(129, 0, 0, 1)), port)
+    SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port)
 }
