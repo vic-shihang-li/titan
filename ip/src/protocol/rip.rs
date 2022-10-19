b1d9318d7fe41a0cc893f0d0866a90c2256e50c2
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-10-18 20:55:42 -0400

    jot down notes for rip handler

--- ip/src/protocol/rip.rs
+++ ip/src/protocol/rip.rs
@@ -3,5 +3,9 @@ use crate::route::ProtocolHandler;
 pub struct RipHandler {}
 
 impl ProtocolHandler for RipHandler {
-    fn handle_packet(&self, payload: &[u8]) {}
+    fn handle_packet(&self, payload: &[u8]) {
+        // TODO:
+        // 1. update the routing table (what APIs does the routing module need to provide?)
+        // 2. send out triggered updates (use `net::iter_links()` and `link.send(rip_message)`)
+    }
 }
